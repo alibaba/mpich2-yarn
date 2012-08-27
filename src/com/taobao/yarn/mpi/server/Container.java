7cf3f95e751c8178153c2694d10ad9158a2b4906
Zhuoluo Yang <zhuoluo.yzl@taobao.com>
2012-08-27 15:01:01 +0800

    Add Container and file distribution

---
+++ src/com/taobao/yarn/mpi/server/Container.java
@@ -0,0 +1,77 @@
+package com.taobao.yarn.mpi.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
+import com.taobao.yarn.mpi.LocalFileUtils;
+
+public class Container {
+
+  /**
+   * @param args
+   * @throws IOException
+   */
+  public static void main(String[] args) throws IOException {
+    printDebugInfo();
+    copyFiles();
+
+    Runtime rt = Runtime.getRuntime();
+    // TODO This need need hack the smpd_cmd_args.c, to add an option set bService
+    // TODO hard coding password
+    final Process pc = rt.exec("smpd -phrase 123456 -debug");
+
+    Thread stdOutThread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        Scanner stdOut = new Scanner(pc.getInputStream());
+        while (stdOut.hasNextLine()) {
+          System.out.println(stdOut.nextLine());
+        }
+      }
+    });
+    stdOutThread.start();
+
+    Thread stdErrThread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        Scanner stdErr = new Scanner(pc.getErrorStream());
+        while (stdErr.hasNextLine()) {
+          System.err.println(stdErr.nextLine());
+        }
+      }
+    });
+    stdErrThread.start();
+  }
+
+  /**
+   * Copy necessary files needed for the MPI program
+   */
+  private static void copyFiles() {
+    Map<String, String> envs = System.getenv();
+    String mpiExecDir = envs.get("MPIEXECDIR");
+    LocalFileUtils.mkdirs(mpiExecDir);
+    File mpiexec = new File("./MPIExec");
+    LocalFileUtils.copyFile(mpiexec, new File(mpiExecDir + "/MPIExec"));
+  }
+
+  /**
+   * Print the environment and working directory information for debugging.
+   * @throws IOException
+   */
+  private static void printDebugInfo() throws IOException {
+    File directory = new File(".");
+    System.err.println(directory.getCanonicalPath());
+    File mpiexec = new File("./MPIExec");
+    System.err.println(mpiexec.getCanonicalPath());
+
+    Map<String, String> envs = System.getenv();
+    Set<Entry<String, String>> entries = envs.entrySet();
+    for (Entry<String, String> entry : entries) {
+      System.err.println("key=" + entry.getKey() + "; value=" + entry.getValue());
+    }
+  }
+}
